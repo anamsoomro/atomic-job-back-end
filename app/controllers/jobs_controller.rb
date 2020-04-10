604f8d839d327fdf7fb07997c34406590a1941cb
anamsoomro <anamsoomroed@gmail.com>
2020-04-10 09:29:20 -0500

    job display with notes and tasks

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,29 @@
 class JobsController < ApplicationController
 
   def index 
-    # byebug
     user = User.find(params[:id])
     jobs = user.jobs 
     render json: jobs
   end
+
+  def show 
+    job = Job.find(params[:id])
+    notes = job.notes 
+    tasks = job.job_tasks
+    render json: {notes: notes, tasks: tasks}
+  end
+
+  def update 
+    job = Job.find(params[:id])
+    job.update(strong_params)
+    render json: job
+  end
+
+  private 
+
+  def strong_params
+    params.require(:job).permit(:status)
+  end
+
 
 end
