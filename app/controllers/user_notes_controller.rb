86d87eb7e6c2a6345db5e5a5f3196c8428ce1912
anamsoomro <anamsoomroed@gmail.com>
2020-04-12 01:17:12 -0500

    notes make, edit, delete

--- app/controllers/user_notes_controller.rb
+++ app/controllers/user_notes_controller.rb
@@ -13,6 +13,21 @@ class UserNotesController < ApplicationController
     render json: note
   end
 
+  def update 
+    note = UserNote.find(params[:id])
+    # user = note.user_id
+    note.update(strong_params)
+    render json: note
+  end
+
+  def destroy
+    note = UserNote.find(params[:id])
+    user = User.find(note.user_id)
+    note.destroy()
+    remainingNotes = user.user_notes
+    render json: remainingNotes
+  end
+
   private 
 
   def strong_params 
